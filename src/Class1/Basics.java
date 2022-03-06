932f6972e1fd1af1b57e6d0d0ea3eca872aefd80
A.W.A <Wakeel@AWAs-MacBook-Air.local>
2022-03-06 14:20:53 -0800

    Adding Class1 notes/practice notes

---
+++ src/Class1/Basics.java
@@ -0,0 +1,42 @@
+package Class1;
+
+public class Basics {
+
+public static void main(String[] args) {
+
+    System.out.println("Hello World!");
+
+    System.out.println("Have a wonderful time in this course");
+}
+
+
+}
+
+
+
+
+/**
+ * another way to add multiline comments
+ */
+
+
+// is a good way to ad multi line comment just remember to close it with same star slash
+//
+/* is another one
+starting point of any compiling is main method psvm string args
+codes outside main methods will not execute
+print it sout it add sout statement
+sout statement must always be in double quotes and in brackets and must end with a semicolon
+console window shows whats been printed
+
+sout used to print in conslole -->  ->
+
+all codes mjust be inside main method in the class
+
+codes written outside the class will give a commpile time error
+codes written in the class but outside the main method will not give compile time error but will not be read
+ */
+
+// used as comment and not considered by Java or read by it
+// no code outside the curly braces of class
+//
